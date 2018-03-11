a86af10a4d25414182f59a3ba0ec5a37203f5296
Eitan Adler <lists@eitanadler.com>
2018-03-11 12:11:43 +0000

    reduce verbosity by default

--- go.sh
+++ go.sh
@@ -10,5 +10,5 @@ then
   sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=/root/.ansible.vault.password "$@"
 else
   echo skipping vault
-  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml -v "$@"
+  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml "$@"
 fi
