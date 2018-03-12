4ea0897f1f56034b7940e3ffbb0296b9b50ad8b8
Eitan Adler <lists@eitanadler.com>
2018-03-12 05:35:35 +0000

    set allow_mount_procfs; make it easier to read

--- jail-specific-settings.sh
+++ jail-specific-settings.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-iocage set enforce_statfs=1 allow_mount=1 allow_mount_devfs=1 allow_mount_nullfs=1 allow_mount_zfs=1 allow_mount_tmpfs=1 sysvmsg=new sysvsem=new sysvshm=new develop
+iocage set \
+  enforce_statfs=1 \
+  allow_mount=1 \
+  allow_mount_devfs=1 \
+  allow_mount_nullfs=1 \
+  allow_mount_zfs=1 \
+  allow_mount_tmpfs=1 \
+  allow_mount_procfs=1  \
+  sysvmsg=new sysvsem=new sysvshm=new \
+  develop
