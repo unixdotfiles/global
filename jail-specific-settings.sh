e8d9cc4d06600d7d22528dab35bac1cd591fab2c
Eitan Adler <lists@eitanadler.com>
2018-03-12 05:21:33 +0000

    Add nullfs for jail; don't set linsysfs which isn't a real propery

--- jail-specific-settings.sh
+++ jail-specific-settings.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-iocage set enforce_statfs=1 allow_mount=1 allow_mount_nullfs=1 allow_mount_linsysfs=1 allow_mount_zfs=1 sysvmsg=new sysvsem=new sysvshm=new develop
+iocage set enforce_statfs=1 allow_mount=1 allow_mount_devfs=1 allow_mount_nullfs=1 allow_mount_zfs=1 sysvmsg=new sysvsem=new sysvshm=new develop
