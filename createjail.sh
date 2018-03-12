a30fddb00f69717ad5c970a55357686d128eee69
Eitan Adler <lists@eitanadler.com>
2018-03-12 06:04:42 +0000

    split createjail into separate lines

--- createjail.sh
+++ createjail.sh
@@ -1,1 +1,14 @@
-iocage create sysvmsg=disable sysvsem=disable sysvshm=disable defaultrouter=192.168.200.1 ip4_addr=none ip6_addr=none bpf=yes dhcp=on devfs_ruleset=5 vnet=on allow_set_hostname=0 boot=on     -r 11.1-RELEASE "$@"
+iocage \
+  create \
+  sysvmsg=disable sysvsem=disable sysvshm=disable \
+  ip4_addr=none \
+  ip6_addr=none \
+  defaultrouter=192.168.200.1 \
+  bpf=yes \
+  dhcp=on \
+  devfs_ruleset=5 \
+  vnet=on \
+  allow_set_hostname=0 \
+  boot=on \
+  -r 11.1-RELEASE \
+  "$@"
