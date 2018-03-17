717c27e8def988850dc29adf17792d0ed33ce299
Eitan Adler <lists@eitanadler.com>
2018-03-17 21:01:15 +0000

    Add more 'develop' specific settings
    Notably: allow setting hostname and creating jails

--- jail-specific-settings.sh
+++ jail-specific-settings.sh
@@ -10,7 +10,14 @@ iocage set \
   allow_mount_zfs=1 \
   allow_mount_tmpfs=1 \
   allow_mount_procfs=1  \
+  allow_chflags=0 \
+  allow_quotas=0 \
+  allow_raw_sockets=0 \
+  allow_set_hostname=1 \
+  allow_socket_af=0 \
+  allow_sysvipc=0 \
   sysvmsg=new sysvsem=new sysvshm=new \
+  children_max=1000 \
   develop
 
 zfs set \
