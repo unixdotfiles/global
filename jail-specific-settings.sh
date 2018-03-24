3fc37da315fc90b365efe64b80e031833fc1301c
Eitan Adler <lists@eitanadler.com>
2018-03-24 20:18:31 +0000

    disconnect develop jail from run

--- jail-specific-settings.sh
+++ jail-specific-settings.sh
@@ -3,25 +3,6 @@
 set -eux
 
 iocage set \
-  enforce_statfs=1 \
-  allow_mount=1 \
-  allow_mount_devfs=1 \
-  allow_mount_nullfs=1 \
-  allow_mount_zfs=1 \
-  allow_mount_tmpfs=1 \
-  allow_mount_procfs=1  \
-  allow_chflags=1 \
-  allow_quotas=1 \
-  allow_raw_sockets=1 \
-  allow_set_hostname=1 \
-  allow_socket_af=1 \
-  allow_sysvipc=1 \
-  sysvmsg=new sysvsem=new sysvshm=new \
-  children_max=1000 \
-  devfs_ruleset=10 \
-  develop
-
-iocage set \
   securelevel=3 \
   bastion
 
