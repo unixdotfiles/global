f019c3e9dad7196883d9d8d87758d6dba840f307
Eitan Adler <lists@eitanadler.com>
2018-04-30 11:31:52 +0000

    Start to add wifi password stuff
    stash since I want to get poudriere further configured

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git pull --rebase || true
 sudo ansible-galaxy install -r requirements.yml
-sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml -v "$@"
+sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=~/.ansible.vault.password -v "$@"
