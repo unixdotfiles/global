d4717ae549b2fcfa8b60fabbc5ec6741ff4fadd4
Eitan Adler <lists@eitanadler.com>
2018-05-10 05:12:49 -0700

    allow skipping vault for non-vault based systems...

--- go.sh
+++ go.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 git pull --rebase || true
 sudo ansible-galaxy install -r requirements.yml
-sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=~/.ansible.vault.password -v "$@"
+
+if [ ! -e ~/.ansible.vault.password ]
+then
+  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=~/.ansible.vault.password -v "$@"
+else
+  echo skipping vault
+  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml -v "$@"
+fi
