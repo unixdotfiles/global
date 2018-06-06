79af24935240b03a8c1a7593ca0f7090246df4e0
Eitan Adler <lists@eitanadler.com>
2018-06-05 22:29:18 -0700

    correct logic inversion

--- go.sh
+++ go.sh
@@ -2,7 +2,7 @@
 git pull --rebase || true
 sudo ansible-galaxy install -r requirements.yml
 
-if [ ! -e ~/.ansible.vault.password ]
+if [ -e ~/.ansible.vault.password ]
 then
   sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=~/.ansible.vault.password -v "$@"
 else
