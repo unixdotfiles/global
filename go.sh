32a042a6043b2c371d4bfc835593c55d93933c43
Eitan Adler <lists@eitanadler.com>
2018-07-02 04:55:49 +0000

    go.sh: be less verbose by default
    The verbosity isn't needed or helpful. It hides other more important
    notices in the noise.

--- go.sh
+++ go.sh
@@ -7,7 +7,7 @@ sudo ansible-galaxy install -r requirements.yml
 
 if [ -e /root/.ansible.vault.password ]
 then
-  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=/root/.ansible.vault.password -v "$@"
+  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=/root/.ansible.vault.password "$@"
 else
   echo skipping vault
   sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml -v "$@"
