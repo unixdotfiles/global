10a1e8c4ec6cdcb0fbcb238ccbba354d1dcb7288
Eitan Adler <eax@twitter.com>
2021-02-25 15:16:24 -0800

    [all] remove old unmaintained wifi password

--- go.sh
+++ go.sh
@@ -5,10 +5,4 @@ trap 'exit 1' INT
 git pull --rebase || true
 sudo ansible-galaxy install -r requirements.yml
 
-if [ -e /root/.ansible.vault.password ]
-then
-  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml --vault-password-file=/root/.ansible.vault.password "$@"
-else
-  echo skipping vault
-  sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml "$@"
-fi
+sudo ansible-playbook --diff -i inventory/$(hostname).yml site.yml "$@"
